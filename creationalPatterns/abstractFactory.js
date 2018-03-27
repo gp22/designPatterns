14a2ca1fafa83b7f1f9f3c1a0303ab29d35272f0
gp22 <gp22@tutanota.com>
2018-03-27 19:36:57 -0400

    complete abstract factory

--- creationalPatterns/abstractFactory.js
+++ creationalPatterns/abstractFactory.js
@@ -3,11 +3,62 @@ const KingOfJoffery = (function() {
 
   }
   KingJoffery.prototype.makeDecision = function() {
-    console.log('King Joffery made a decision');
+
   };
   KingJoffery.prototype.marry = function() {
-    console.log('King Joffery got married');
+
   };
 
   return KingJoffery;
 })();
+
+const LordTywin = (function() {
+  function LordTywin() {
+
+  }
+  LordTywin.prototype.makeDecision = function() {
+
+  };
+
+  return LordTywin;
+})();
+
+const LannisterFactory = (function() {
+  function LannisterFactory() {
+
+  }
+  LannisterFactory.prototype.getKing = function() {
+    return new KingJoffery();
+  };
+  LannisterFactory.prototype.getHandOfTheKing = function() {
+    return new LordTywin();
+  };
+
+  return LannisterFactory;
+})();
+
+/*
+  Implementation
+*/
+const CourtSession = (function() {
+  function CourtSession(abstractFactory) {
+    this.abstractFactory = abstractFactory;
+    this.COMPLAINT_THRESHOLD = 10;
+  }
+  CourtSession.prototype.complaintPresented = function(complaint) {
+    if (complaint.severity < this.COMPLAINT_THRESHOLD) {
+      this.abstractFactory.getHandOfTheKing().makeDecision();
+    } else {
+      this.abstractFactory.getKing().makeDecision();
+    };
+
+    return CourtSession;
+  }
+})();
+
+/*
+  Usage
+*/
+const courtSession1 = new CourtSession(new LannisterFactory());
+courtSession1.complaintPresented({ severity: 8 });
+courtSession1.complaintPresented({ severity: 12 });
